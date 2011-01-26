4ae9aa3077652d330de8ec8d692d1123c5f9c229
Patrick Spendrin <ps_ml@gmx.de>
2011-01-26 17:00:47 +0100

    update the usage

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@
 using namespace std;
 
 void usage() {
-    cout << "a" << endl \
-         << "b" << endl \
-         << "c" << endl \
-         << "d" << endl \
-         << "e" << endl;
+    cout << "kcwsh is a wrapper Shell for cmd.exe which understands ansi " << endl \
+         << "escape sequences for some functionality." << endl \
+         << endl << "options:" << endl \
+         << "-h, --help" << "\tDisplays this help message." << endl \
+         << "-d, --debug" << "\tswitches on debug mode" << endl \
+         << endl << "Copyright (C) by Patrick Spendrin 2011" << endl \
+         << "Redistributable under GPLv3" << endl;
 }
+
 int main(int argc, char **argv) {
     vector<string> args(argv, argv + argc);
 
@@ -26,6 +29,7 @@ int main(int argc, char **argv) {
             return 1;
         }
     }
+
     ClientHandler handler("C:\\Windows\\SysWOW64\\cmd.exe");
     PipeHandler in(PipeHandler::STDIN_PIPE);
     PipeHandler out(PipeHandler::STDOUT_PIPE);
