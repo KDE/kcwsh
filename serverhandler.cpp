a5242ee80ed463ad251dc54159afc87be7de351e
Patrick Spendrin <ps_ml@gmx.de>
2011-02-04 17:48:31 +0100

    use the eventloop in the client thread as well

--- serverhandler.cpp
+++ serverhandler.cpp
@@ -1,18 +1,32 @@
 #include "serverhandler.h"
 #include <windows.h>
 
-ServerHandler::ServerHandler() {
+ServerHandler::ServerHandler()
+  : KcwEventLoop(::CreateEvent(NULL, FALSE, FALSE, "KcwServerHandler")), 
+    m_thread(NULL) {
 }
 
 void ServerHandler::connect() {
     char tmp[1024];
     DWORD dwProcessId = ::GetCurrentProcessId();
+    DWORD dwThreadId = 0;
     sprintf(tmp, "kcwsh-%x", dwProcessId);
     m_test.open(tmp);
     
     sprintf(tmp, "running in process with id: %i %i", dwProcessId, *m_test);
     OutputDebugString(tmp);
+    m_thread = ::CreateThread(NULL, 0, monitorThreadStatic, reinterpret_cast<void*>(this), 0, &dwThreadId);
 }
 
 void ServerHandler::disconnect() {
+    // quit the eventloop
+    quit();
+    OutputDebugString("quitting the eventloop!");
+    ::WaitForSingleObject(m_thread, 10000);
+    OutputDebugString("disconnected!");
+}
+
+DWORD WINAPI ServerHandler::monitorThreadStatic(LPVOID lpParameter) {
+    ServerHandler* pServerHandler = reinterpret_cast<ServerHandler*>(lpParameter);
+    return pServerHandler->exec();
 }
--- serverhandler.h
+++ serverhandler.h
@@ -2,14 +2,19 @@
 #define serverhandler_h
 
 #include "sharedmemory.h"
+#include "kcweventloop.h"
 
-class ServerHandler {
+class ServerHandler : public KcwEventLoop {
     public:
         ServerHandler();
         void connect();
         void disconnect();
         
     private:
+        static DWORD WINAPI monitorThreadStatic(LPVOID lpParameter);
+        DWORD monitorThread();
+
+        HANDLE m_thread;
         SharedMemory<int> m_test;
 };
 
