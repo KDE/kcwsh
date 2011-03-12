03f220c849a9a4b28b31f0aafb0379fb61536675
Patrick Spendrin <ps_ml@gmx.de>
2011-03-12 23:20:11 +0100

    add some documentation for kcwapp

--- kcw/kcwapp.h
+++ kcw/kcwapp.h
@@ -3,11 +3,18 @@
 
 #include "kcweventloop.h"
 
+/**
+* @author Patrick Spendrin
+* @date 2011
+* @brief KcwApp is the Application class which handles the Event distribution and the event loop.
+* @details KcwApp is a simple wrapper around KcwEventLoop, which has no additional functionality
+* until now. For future extensions, this class should be used though.
+*/
 class KcwApp : public KcwEventLoop {
-    /**
-    * KcwApp is the Application class which handles the Event distribution and the event loop.
-    */
     public:
+        /**
+        * construct a default KcwApp object.
+        */
         KcwApp();
 };
 #endif /* kcwapp_h */
