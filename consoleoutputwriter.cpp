d35c2369c0cdd821640e076903e05ad503b6a99d
Patrick Spendrin <ps_ml@gmx.de>
2013-07-11 12:28:30 +0200

    implement console output

--- consoleoutputwriter.cpp
+++ consoleoutputwriter.cpp
@@ -8,11 +8,49 @@ ConsoleOutputWriter::ConsoleOutputWriter()
 : OutputWriter() {
 }
 
+ConsoleOutputWriter::~ConsoleOutputWriter() {
+    restoreConsole();
+}
+
 void ConsoleOutputWriter::writeData() {
     KcwDebug() << "writing new Data to output!";
+    COORD bufferOrigin; bufferOrigin.X = 0; bufferOrigin.Y = 0;
+    SMALL_RECT sr;
+    sr.Top = 1; sr.Left = 0; sr.Right = m_bufferSize->X; sr.Bottom = m_bufferSize->Y + 1;
+    WriteConsoleOutput(m_screen, m_output.data(), *m_bufferSize.data(), bufferOrigin, &sr);
+}
+void ConsoleOutputWriter::saveConsole() {
+    KcwDebug() << "saving console info";
+    m_screenSave = GetStdHandle(STD_OUTPUT_HANDLE);
+    m_screen = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(m_screen);
+}
+
+void ConsoleOutputWriter::restoreConsole() {
+    KcwDebug() << "restoring console info";
+    SetConsoleActiveScreenBuffer(m_screenSave);
 }
 
 void ConsoleOutputWriter::init() {
     OutputWriter::init();
+
+    // save original state
+    saveConsole();
+
+    // now change our console size
+    CONSOLE_SCREEN_BUFFER_INFOEX csbi;
+    ZeroMemory(&csbi, sizeof(CONSOLE_SCREEN_BUFFER_INFOEX));
+    csbi.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+    KcwDebug() << "getting console info:" << GetConsoleScreenBufferInfoEx(m_screen, &csbi);
+    COORD currentCoords = *m_bufferSize;
+    KcwDebug() << "current size of the buffer:" << currentCoords.X << "X" << currentCoords.Y;
+    int headerLines = 2;
+    currentCoords.Y += headerLines;
+    csbi.dwSize = currentCoords;
+    SMALL_RECT sr;
+    sr.Left = 0; sr.Top = headerLines; sr.Right = currentCoords.X; sr.Bottom = currentCoords.Y + headerLines;
+    csbi.dwMaximumWindowSize = currentCoords;
+    csbi.srWindow = sr;
+    KcwDebug() << "setting Console info:" << SetConsoleScreenBufferInfoEx(m_screen, &csbi);
     addCallback(m_bufferChanged.handle(), CB(ConsoleOutputWriter::writeData));
 }
--- consoleoutputwriter.h
+++ consoleoutputwriter.h
@@ -8,10 +8,16 @@
 class ConsoleOutputWriter : public KcwSH::OutputWriter {
     public:
         ConsoleOutputWriter();
+        ~ConsoleOutputWriter();
 
         void init();
 
         virtual KCW_CALLBACK(ConsoleOutputWriter, writeData);
     private:
+        void saveConsole();
+        void restoreConsole();
+
+        HANDLE m_screen;
+        HANDLE m_screenSave;
 };
 #endif /* consoleoutputwriter */
