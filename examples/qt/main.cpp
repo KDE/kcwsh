7d4f1a0a1441ebe9ce8026b17945bfc9f6e564a0
Patrick Spendrin <ps_ml@gmx.de>
2013-07-17 00:32:34 +0200

    implement a small application with the Qt frontend

--- examples/qt/main.cpp
+++ examples/qt/main.cpp
@@ -1,7 +1,11 @@
 #include "qterminalwidget.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char**argv) {
     QApplication app(argc, argv);
+    KcwSH::QtFrontend::TerminalWidget term;
+    term.show();
+    qDebug() << "shown!";
     return app.exec();
 }
