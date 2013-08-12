ade66df01fd640263b8a7928645826314e7c4b68
Patrick Spendrin <ps_ml@gmx.de>
2013-08-13 00:26:54 +0200

    use the exitEvent when unloading the dll to make sure it is called

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,19 +1,31 @@
 #include <windows.h>
 
 #include <kcwdebug.h>
+#include <kcwnotifier.h>
+#include <string>
 
 HINSTANCE s_module = 0;
+KcwNotifier s_notifier;
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /* lpvReserved */ ) {
     s_module = hInstance;
     switch(dwReason) {
         case DLL_PROCESS_ATTACH:
         {
+            std::wstringstream wss;
+            wss << L"kcwsh-exitEvent-" << GetCurrentProcessId();
+            s_notifier.open(wss.str());
             break;
         }
         case DLL_PROCESS_DETACH:
         {
-            KcwDebug() << "detaching from process!";
+            KcwDebug() << "detaching from process!" << ::GetCurrentProcessId();
+            s_notifier.notify();
+            break;
+        }
+        case DLL_THREAD_ATTACH:
+        {
+            KcwDebug() << "attaching thread!";
             break;
         }
         case DLL_THREAD_DETACH:
