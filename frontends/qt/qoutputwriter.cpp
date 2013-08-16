dde5564edc1bca5f42dde89f7178d75a2349185a
Patrick Spendrin <ps_ml@gmx.de>
2013-08-17 00:49:14 +0200

    simplify colors and implement a simple cursor for the Qt widget

--- frontends/qt/qoutputwriter.cpp
+++ frontends/qt/qoutputwriter.cpp
@@ -17,9 +17,8 @@
 using namespace KcwSH::QtFrontend;
 
 
-QPen penForAttribute(int attr)
+QColor colorForAttribute(int attr)
 {
-    int shortAttributes = attr & 0xf;
     QColor colorArray[16];
     colorArray[0x0] = QColor(0x00, 0x00, 0x00);
     colorArray[0x1] = QColor(0x00, 0x00, 0x80);
@@ -38,31 +37,7 @@ QPen penForAttribute(int attr)
     colorArray[0xe] = QColor(0xff, 0xff, 0x00);
     colorArray[0xf] = QColor(0xff, 0xff, 0xff);
 
-    return QPen(colorArray[shortAttributes]);
-}
-
-QBrush brushForAttribute(int attr)
-{
-    int shortAttributes = (attr & 0xf0) >> 4;
-    QColor colorArray[16];
-    colorArray[0x0] = QColor(0x00, 0x00, 0x00);
-    colorArray[0x1] = QColor(0x00, 0x00, 0x80);
-    colorArray[0x2] = QColor(0x00, 0x80, 0x00);
-    colorArray[0x3] = QColor(0x00, 0x80, 0x80);
-    colorArray[0x4] = QColor(0x80, 0x00, 0x00);
-    colorArray[0x5] = QColor(0x80, 0x00, 0x80);
-    colorArray[0x6] = QColor(0x80, 0x80, 0x00);
-    colorArray[0x7] = QColor(0xc0, 0xc0, 0xc0);
-    colorArray[0x8] = QColor(0x80, 0x80, 0x80);
-    colorArray[0x9] = QColor(0x00, 0x00, 0xff);
-    colorArray[0xa] = QColor(0x00, 0xff, 0x00);
-    colorArray[0xb] = QColor(0x00, 0xff, 0xff);
-    colorArray[0xc] = QColor(0xff, 0x00, 0x00);
-    colorArray[0xd] = QColor(0xff, 0x00, 0xff);
-    colorArray[0xe] = QColor(0xff, 0xff, 0x00);
-    colorArray[0xf] = QColor(0xff, 0xff, 0xff);
-
-    return QBrush(colorArray[shortAttributes]);
+    return colorArray[attr];
 }
 
 QtOutputWriter::QtOutputWriter(TerminalWidgetTerminal* term)
@@ -96,11 +71,12 @@ void QtOutputWriter::paintOutput(QPainter* p, const QRectF& r) {
             oldattr = buffer[linenum * maxcolnum].Attributes;
             for(int colnum = 0; colnum < maxcolnum; colnum++) { // go through each column
                 const int curattr = buffer[linenum * maxcolnum + colnum].Attributes;
+                if(curattr & COMMON_LVB_UNDERSCORE) qDebug() << __FUNCTION__ << "(" << linenum << ", " << colnum << ")";
                 if(curattr != oldattr) { // in case the attribute changes, we need to adjust our QPen style and draw the text
                     *arrayptr = L'\0';
                     tempText = QString::fromUtf16(reinterpret_cast<ushort*>(curLine));
-                    p->setPen(penForAttribute(oldattr));
-                    p->setBackground(brushForAttribute(oldattr));
+                    p->setPen(colorForAttribute(oldattr & 0xf));
+                    p->setBackground(QBrush(colorForAttribute((oldattr >> 4) & 0xf)));
                     p->drawText(r.x() + (curLine - beginLine) * fontWidth, r.y() + linenum * fontHeight, 
                                 r.x() + colnum * fontWidth,                r.y() + (linenum + 1) * fontHeight, 
                                 Qt::AlignLeft | Qt::AlignTop, tempText);
@@ -112,12 +88,23 @@ void QtOutputWriter::paintOutput(QPainter* p, const QRectF& r) {
             }
             *arrayptr = L'\0';
             tempText = QString::fromUtf16(reinterpret_cast<ushort*>(curLine));
-            p->setPen(penForAttribute(oldattr));
-            p->setBackground(brushForAttribute(oldattr));
+            p->setPen(colorForAttribute(oldattr & 0xf));
+            p->setBackground(QBrush(colorForAttribute((oldattr >> 4) & 0xf)));
             p->drawText(r.x() + (curLine - beginLine) * fontWidth, r.y() + linenum * fontHeight, 
                         r.width(),                                 r.y() + (linenum + 1) * fontHeight, 
                         Qt::AlignLeft | Qt::AlignTop, tempText);
         };
+
+        // overwrite the character where the cursor is and simply invert the character colors
+        const COORD cp = *m_cursorPosition;
+        WCHAR s[2]; s[0] = buffer[cp.Y * maxcolnum + cp.X].Char.UnicodeChar; s[1] = 0;
+        int attr = buffer[cp.Y * maxcolnum + cp.X].Attributes;
+        tempText = QString::fromUtf16(reinterpret_cast<ushort*>(s));
+        p->setPen(colorForAttribute((attr >> 4) & 0xf));
+        p->setBackground(QBrush(colorForAttribute(attr & 0xf)));
+        p->drawText(r.x() + cp.X * fontWidth      , r.y() + cp.Y * fontHeight,
+                    r.x() + (cp.X + 1) * fontWidth, r.y() + (cp.Y + 1) * fontHeight,
+                    Qt::AlignLeft | Qt::AlignTop, tempText);
         delete[] buffer;
         delete[] array;
     }
