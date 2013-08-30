d77b56de2dfc232f9a8731004888c2eb6b795575
Patrick Spendrin <ps_ml@gmx.de>
2013-08-31 00:59:10 +0200

    restrict size of Widget on startup

--- examples/qt/main.cpp
+++ examples/qt/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char**argv) {
     QApplication app(argc, argv);
     KcwSH::QtFrontend::TerminalWidget term;
+    term.setMinimumSize(560, 350);      // the standard terminal size of 80x25
     term.show();
     qDebug() << "shown!";
     return app.exec();
